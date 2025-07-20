0c588f2d6ba6027316e8adc828affc06d150b573
Steven Jaimes Galeano <steinu037@gmail.com>
2025-07-20 14:55:39 -0500

    opciones a escoger

--- app/page.tsx
+++ app/page.tsx
@@ -54,14 +54,13 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
+            Agrega la o las imágenes que deseas cambiar el formato.
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Escoge el formato a cambiarlas.
+          </li>
+          <li className="tracking-[-.01em]">
+            Escoge la carpeta que deseas las guarden.
           </li>
         </ol>
 
@@ -73,16 +72,9 @@ export default function Home() {
             multiple
             accept=".avif"
             onChange={nameFile}
+            title = "Choose a video please"
           />
           <button onClick={imageChange}>cambiar imagenes</button>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
